0022092fcbfca4f26f67d121770fae608eff7042
deepaksinghgusain <deepakgusain2022@gmail.com>
2025-10-01 13:34:51 +0000

    changes1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apibaseurl: 'http://localhost:3000',
-  imageEndPoint: 'http://localhost:3000',
+  apibaseurl: 'https://admin.cmeavenue.com',
+  imageEndPoint: 'https://admin.cmeavenue.com',
   twilioAgentIdentity: "cpetest000@outlook.com",
   rotoken: 'c8b1d87cd63f1bddf04e81c6e8f9d80d773afc7db69088a72d20bf367dbbdc2f6ef77a005e3ef8ceaa3e0ac42e180211dacbb5ba831268b3a4f2b8af0906957a7bf011dca558522131f15d7eae6055c9df7ba3653b03c12ff2dafed99244d8f5607ea8392ada2f0ae73df23c2202ec58c982a01cfeda1bb1e1384cc03ef181b6',
   
